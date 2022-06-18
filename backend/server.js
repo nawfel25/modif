aa538e9c0f22a267e75e63d6ad2c20dcb6d97df6
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 09:11:37 +0800

    initialize local server config

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('gegeg');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.status(201).json({ message: "eta gegeg!" });
+});
+
+app.listen(PORT, () => console.log(`server started at ${PORT}`));
