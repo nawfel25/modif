2b8956a76090584cb6bfc9244b7df1350ed456bd
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 23:24:00 +0800

    initialize home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>What brings you here?</h1>
+        <p>Please choose from the options below</p>
+      </section>
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create new ticket
+      </Link>
+      <Link to="/tickets" className="btn btn-block">
+        <FaTicketAlt /> View my tickets
+      </Link>
+    </>
+  );
 }
 
 export default Home;
