97942ae92fd204dbf03d3bfd7d583af0e3088438
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 12:44:55 +0800

    added login module that implements JWT for authentication

--- commands.js
+++ commands.js
@@ -1,5 +1,6 @@
 //  //dependencies
 //  npm install express dotenv mongoose colors bcryptjs
 //  npm install --save express-async-handler
+//  npm install jsonwebtoken
 //  //dev dependencies
 //  npm install -D nodemon
