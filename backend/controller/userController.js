0dc89c7f02360d6596570ae5b718552b16fe2892
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 10:06:44 +0800

    added async handler

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,8 @@
+const asyncHandler = require("express-async-handler");
 // @desc    Register new user
 // @route   /api/users
 // @access  Public
-const userRegister = (req, res) => {
+const userRegister = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   //validation
   if (!name || !email || !password) {
@@ -9,13 +10,13 @@ const userRegister = (req, res) => {
     throw new Error("fields are required");
   }
   res.send("registration route");
-};
+});
 // @desc    Login a user
 // @route   /api/users/login
 // @access  Public
-const userLogin = (req, res) => {
+const userLogin = asyncHandler(async (req, res) => {
   res.send("login route");
-};
+});
 
 module.exports = {
   userRegister,
--- commands.js
+++ commands.js
@@ -1,4 +1,5 @@
 //  //dependencies
 //  npm install express dotenv mongoose colors bcryptjs
+//  npm install --save express-async-handler
 //  //dev dependencies
 //  npm install -D nodemon
