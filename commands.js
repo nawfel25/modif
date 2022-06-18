abd0384ebfaa60a7f7fab92e4c8ba05822e359be
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 19:52:32 +0800

    added css classes

--- commands.js
+++ commands.js
@@ -3,5 +3,7 @@
 //  npm install --save express-async-handler
 //  npm install jsonwebtoken
 //  npm install concurrently
+//  npm i react-router-dom react-toastify axios react-icons reac-modal
+
 //  //dev dependencies
 //  npm install -D nodemon
