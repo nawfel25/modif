e3092f210473e5f5ac9e7f0f00dfbc19729b3b0d
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 09:24:54 +0800

    initialize route directory

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
   res.status(201).json({ message: "eta gegeg!" });
 });
+
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
