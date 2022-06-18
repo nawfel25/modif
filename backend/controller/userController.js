9d734c2b798fc3eb2c3fd1dc7913f0aa39701744
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 18:25:46 +0800

    added auth middleware for protecting private routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -66,6 +66,18 @@ const userLogin = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get current user
+// @route   /api/users/me
+// @access  private
+const getMe = (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+};
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -75,4 +87,5 @@ const generateToken = (id) => {
 module.exports = {
   userRegister,
   userLogin,
+  getMe,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { userRegister, userLogin } = require("../controller/userController");
+const {
+  userRegister,
+  userLogin,
+  getMe,
+} = require("../controller/userController");
+
+//jwt middleware
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", userRegister);
 router.post("/login", userLogin);
+router.get("/me", protect, getMe);
 
 module.exports = router;
