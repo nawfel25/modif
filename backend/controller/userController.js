30831364dba4fc0d7f1d8e00bdf298a0051db939
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 10:03:34 +0800

    added errorMiddleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,18 @@
+// @desc    Register new user
+// @route   /api/users
+// @access  Public
 const userRegister = (req, res) => {
+  const { name, email, password } = req.body;
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("fields are required");
+  }
   res.send("registration route");
 };
-
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const userLogin = (req, res) => {
   res.send("login route");
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+//enable json and urlendocded values on request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(201).json({ message: "eta gegeg!" });
 });
 
+//users route
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
