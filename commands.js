343ee71ac9233f629f45cc04b5b8ae6bae59b599
Jepoy Factoran <factoran20@gmail.com>
2022-06-18 19:43:32 +0800

    added concurrently to run frontend and backen simultaneously

--- commands.js
+++ commands.js
@@ -2,5 +2,6 @@
 //  npm install express dotenv mongoose colors bcryptjs
 //  npm install --save express-async-handler
 //  npm install jsonwebtoken
+//  npm install concurrently
 //  //dev dependencies
 //  npm install -D nodemon
